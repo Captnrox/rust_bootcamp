2ff5ebb05588e9bdef786fbae9cfefc429027916
Eduardo <edurocadas@gmail.com>
2025-03-22 12:53:09 +0000

    updated: user

--- user/src/main.rs
+++ user/src/main.rs
@@ -27,15 +27,23 @@ impl User {
             GLOBAL_ID
         }
     }
+
+
+    fn update(&mut self, new_email: String) {
+        self.mail = new_email;  // Update only the `mail` field
+    }
 }
 
 
 fn main(){
-    let user = User::new(String::from("user@example.com"), String::from("john_doe"));
-    let user2 = User::new(String::from("user2@example.com"), String::from("johnny_doe"));
+    let mut user = User::new(String::from("user@example.com"), String::from("john_doe"));
+    let mut user2 = User::new(String::from("user2@example.com"), String::from("johnny_doe"));
+
+    user.update(String::from("new_email@example.com"));
 
     println!("User ID: {}", user.id);
     println!("Username: {}", user.user);
+    println!("Email: {}", user.mail);
 
     println!("User ID: {}", user2.id);
     println!("Username: {}", user2.user);
